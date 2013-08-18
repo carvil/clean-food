98eb540a9970836811786971b8a5960e51ef3046
Carlos Vilhena <carlosvilhena@gmail.com>
2013-08-18 17:24:13 +0100

    add migration for the authority table

---
+++ src/migrations/20130818170925_local_authority.clj
@@ -0,0 +1,27 @@
+(ns migrations.20130818170925-local-authority
+  (:use [korma.db]
+        [korma.core])
+  (:require [clojure.java.jdbc :as sql]
+            [uk-food-hygiene.models.db :as db]))
+
+(defn up
+  "Migrates the database up to version 20130818170925."
+  []
+  (println "migrations.20130818170925-local-authority up...")
+  (sql/with-connection db/db-spec
+    (sql/create-table
+      :local_authority
+        [:id          "SERIAL PRIMARY KEY"]
+        [:business_id :integer]
+        [:code        :integer]
+        [:name        "varchar(200)"]
+        [:website     "varchar(200)"]
+        [:email       "varchar(200)"]
+        [:created_at  "TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()"])))
+
+(defn down
+  "Migrates the database down from version 20130818170925."
+  []
+  (println "migrations.20130818170925-local-authority down...")
+  (sql/with-connection db/db-spec
+    (sql/drop-table :local_authority)))
