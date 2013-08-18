1ee91e7e34828293cb63b0ef3db3fa937601108d
Carlos Vilhena <carlosvilhena@gmail.com>
2013-08-18 18:08:20 +0100

    adding migration for the business-type table

---
+++ src/migrations/20130818172446_business_type.clj
@@ -0,0 +1,24 @@
+(ns migrations.20130818172446-business-type
+  (:use [korma.db]
+        [korma.core])
+  (:require [clojure.java.jdbc :as sql]
+            [uk-food-hygiene.models.db :as db]))
+
+(defn up
+  "Migrates the database up to version 20130818172446."
+  []
+  (println "migrations.20130818172446-business-type up...")
+  (sql/with-connection db/db-spec
+    (sql/create-table
+      :business_type
+        [:id               "SERIAL PRIMARY KEY"]
+        [:business_type_id :integer]
+        [:name             "varchar(200)"]
+        [:created_at  "TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()"])))
+
+(defn down
+  "Migrates the database down from version 20130818172446."
+  []
+  (println "migrations.20130818172446-business-type down...")
+  (sql/with-connection db/db-spec
+    (sql/drop-table :business_type)))
