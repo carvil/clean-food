2e4dd5147452845438b549e4217314b8b10c3b0d
Carlos Vilhena <carlosvilhena@gmail.com>
2013-09-15 15:35:34 +0100

    add create and select functions to the models module

--- src/uk_food_hygiene/models/db.clj
+++ src/uk_food_hygiene/models/db.clj
@@ -8,3 +8,43 @@
          :password ""})
 
 (defdb db db-spec)
+
+; CREATE
+
+(defn create-rating [rating]
+  (insert :ratings
+          (values rating)))
+
+(defn create-local-authority [local-authority]
+  (insert :local_authorities
+          (values local-authority)))
+
+(defn create-business-type [business-type]
+  (insert :business_types
+          (values business-type)))
+
+(defn create-establishment [establishment]
+  (insert :establishments
+          (values establishment)))
+
+; SELECT
+
+(defn get-establishment [id]
+  (first (select :establishments
+                 (where {:id id})
+                 (limit 1))))
+
+(defn get-rating [id]
+  (first (select :ratings
+                 (where {:id id})
+                 (limit 1))))
+
+(defn get-local-authority [id]
+  (first (select :local_authorities
+                 (where {:id id})
+                 (limit 1))))
+
+(defn get-business-type [id]
+  (first (select :business_types
+                 (where {:id id})
+                 (limit 1))))
